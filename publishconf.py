a53dfbc5a4b472199f2f882a50684e19183eb93c
ptanguy <ph.tanguy@gmail.com>
2015-12-16 22:09:50 +0100

    mod site url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://ptanguy.github.io/blog.ptanguy.fr/'
+SITEURL = 'http://www.blog.ptanguy.fr'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
